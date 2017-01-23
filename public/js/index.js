afcc890ea9d940ee5e33380c7af543e19b3fe833
nazgul <tamas.kadlecsik@gmail.com>
2017-01-23 21:46:11 +0100

    feat: update names

--- public/js/index.js
+++ public/js/index.js
@@ -5,15 +5,19 @@ var idsAndHashes = {
 }
 
 window.onload = function () {
-  var keepScrolling = document.querySelector('.keep-scrolling')
-  keepScrolling.onclick = scrollTo('invitation')
-  var invitationHeaderLink = document.querySelector('#navbar > ul > li:nth-child(2)')
-  invitationHeaderLink.onclick = scrollTo('invitation')
-
-  var navHome = document.querySelector('#navbar > ul > li:nth-child(1)')
+  var navHome = document.querySelector('#navbar-home')
   navHome.onclick = scrollTo('home')
   var title = document.querySelector('.navbar-header')
   title.onclick = scrollTo('home')
+
+  var invitationHeaderLink = document.querySelector('#navbar-invitation')
+  invitationHeaderLink.onclick = scrollTo('invitation')
+  var keepScrolling = document.querySelector('.keep-scrolling')
+  keepScrolling.onclick = scrollTo('invitation')
+
+  var langSelector = document.querySelector('#navbar-lang')
+  var targetLang = langSelector.dataset.targetLang
+  langSelector.onclick = toggleLanguage(targetLang)
 }
 
 function scrollTo (id) {
@@ -27,4 +31,20 @@ function scrollTo (id) {
     window.location.hash = id
     evt.preventDefault()
   }
+}
+
+function toggleLanguage (targetLang) {
+  return function onStartToggle (evt) {
+    if (targetLang === 'en') {
+      updateQueryString('lang', 'en')
+    } else {
+      updateQueryString('lang', 'hu')
+    }
+    evt.preventDefault()
+  }
+}
+
+function updateQueryString(key, value) {
+  var regex = new RegExp('(' + key + '=)\\w+')
+  window.location.search = window.location.search.replace(regex, '$1' + value)
 }
