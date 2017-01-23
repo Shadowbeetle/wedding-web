9f1b107a951ed93e006e0135e11ef017f48bf4f7
nazgul <tamas.kadlecsik@gmail.com>
2017-01-23 06:15:50 +0100

    feat: add keep scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,22 @@
 'use strict'
+window.onload = function () {
+  var keepScrolling = document.querySelector('.keep-scrolling')
+  var aboutUs = document.querySelector('#about-us')
+
+  keepScrolling.onclick = function scrollNext (evt) {
+    aboutUs.scrollIntoView({
+      behavior: 'smooth'
+    })
+
+    window.location.hash = 'about-us'
+  }
+
+  var home = document.querySelector('.wedding-home')
+  var navHome = document.querySelector('#navbar > ul > li:nth-child(1)')
+
+  navHome.onclick = function scrollHome (evt) {
+    home.scrollIntoView({
+      behavior: 'smooth'
+    })
+  }
+}
