c22a7d69e29bb3efb3c826837d2bdc70909119ae
nazgul <tamas.kadlecsik@gmail.com>
2017-01-28 18:03:00 +0100

    refactor: replace ids with random strings

--- script/createQrCodes.js
+++ script/createQrCodes.js
@@ -1,14 +1,16 @@
+#!/usr/bin/env node
 'use strict'
 const qr = require('qr-image')
-const guestListObj = require('../server/models/greeting.json')
+const yaml = require('js-yaml')
 const fs = require('fs');
+const guestListObj = yaml.safeLoad(fs.readFileSync('../server/models/greeting.yaml', 'utf8'))
 const dirName = '../qrCodes';
 
 if (!fs.existsSync(dirName)){
   fs.mkdirSync(dirName);
 }
 
-const BASE_URL = 'https://kadlecsik-wedding-web.herokuapp.com/'
+const BASE_URL = 'http://anna-tamas-eskuvo.com/'
 
 for (let guestId in guestListObj) {
   let lang = (guestId === '144' || guestId === '145') ? 'en' : 'hu'
