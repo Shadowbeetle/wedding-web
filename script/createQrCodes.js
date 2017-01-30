a8b17435fc8e8cc4aa58f40a6d33d58c711e57e9
nazgul <tamas.kadlecsik@gmail.com>
2017-01-30 22:25:02 +0100

    feat: update qr code generator

--- script/createQrCodes.js
+++ script/createQrCodes.js
@@ -3,6 +3,7 @@
 const qr = require('qr-image')
 const yaml = require('js-yaml')
 const fs = require('fs');
+const path = require('path')
 const guestListObj = yaml.safeLoad(fs.readFileSync('../server/models/guestList.yaml', 'utf8'))
 const dirName = '../qrCodes';
 
@@ -14,7 +15,7 @@ const BASE_URL = 'http://anna-tamas-eskuvo.com/'
 
 for (let guestId in guestListObj) {
   let lang = (guestId === '144' || guestId === '145') ? 'en' : 'hu'
-  let url = `${BASE_URL}?lang=${lang}&guest=${guestId}`
+  let url = `${BASE_URL}/guest/${guestId}?lang=${lang}`
   let fileName = `${guestListObj[guestId].join('_')}_${guestId}`
 
   let qrSvg = qr.image(url, { type: 'svg' })
@@ -23,3 +24,5 @@ for (let guestId in guestListObj) {
   let qrPng = qr.image(url, { type: 'png' })
   qrPng.pipe(require('fs').createWriteStream(`${dirName}/${fileName}.png`))
 }
+
+console.log('QR codes can be found in', path.join(__dirname, dirName))
