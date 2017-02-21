627220338a0a6d69953ef7839b54a274562a3053
nazgul <tamas.kadlecsik@gmail.com>
2017-02-21 19:55:13 +0100

    fix(setupServer): keep running on error

--- server/util/setupServer.js
+++ server/util/setupServer.js
@@ -1,19 +1,52 @@
 'use strict'
 
-module.exports = function setupServer (app) {
+module.exports = function setupServer(app) {
   return {
+    _listening: false,
+
     listen (port) {
+      this._listening = true
+
       return new Promise((resolve, reject) => {
+        const previousServer = this._server
+
         this._server = app.listen(port, (err) => {
-          if (err) reject(err)
+          if (err) return reject(err)
+          return resolve()
+        })
+
+        // In order to catch errors we have to listen to error events on the returned httpServer
+        this._server.on('error', (err) => {
+          // If the port is in use by us than we restart the server
+          if (err.code === 'EADDRINUSE' && err.port === port && this._listening) {
+            return this._closeOld(previousServer)
+              .then(() => {
+                return this.listen(port)
+              })
+              .then(resolve)
+              .catch(reject)
+          } else {
+            return reject(err)
+          }
+        })
+      })
+    },
+
+    close () {
+      return new Promise((resolve, reject) => {
+        this._server.close((err) => {
+          if (err) return reject(err)
+          this._listening = false
           return resolve()
         })
       })
     },
-    close () {
+
+    _closeOld (server) {
       return new Promise((resolve, reject) => {
-        this._server.close((err) => {
-          if (err) reject(err)
+        server.close((err) => {
+          if (err) return reject(err)
+          this._listening = false
           return resolve()
         })
       })
