8806a863fe813fc06c431b72e6b5d761ba16f3d4
nazgul <tamas.kadlecsik@gmail.com>
2017-02-21 18:39:50 +0100

    feat: add fetch decorator to get cookies

---
+++ server/util/fetchGetCookies.js
@@ -0,0 +1,29 @@
+'use strict'
+const cookie = require('cookie')
+
+module.exports = function addFetchCookies (fetch) {
+  return function fetchWithCookies (url, opts) {
+    return fetch(url, opts)
+      .then((res) => {
+        const setCookieHeader = res.headers._headers['set-cookie']
+        if (setCookieHeader) {
+          res.cookies = createCookiesObject(setCookieHeader)
+        }
+        return res
+      })
+  }
+}
+
+function createCookiesObject(setCookieHeader) {
+  return setCookieHeader.reduce(parseCookieStringIntoCookies, {})
+}
+
+function parseCookieStringIntoCookies (cookies, cookieString) {
+  const cookieObj = cookie.parse(cookieString)
+  const keys = Object.keys(cookieObj)
+
+  cookies[keys[0]] = Object.assign(cookieObj, { value: cookieObj[keys[0]] })
+  delete cookieObj[keys[0]]
+
+  return cookies
+}
